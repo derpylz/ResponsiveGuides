ed46517b196183973609e1ccbc348243d7d29651
derpylz <nils.trost@hotmail.de>
2024-03-21 18:38:13 +0100

    Initial commit.

---
+++ R/ResponsiveGuides.R
@@ -0,0 +1,46 @@
+#' @import htmlwidgets
+#' @export
+responsiveGuide <- function(variables, colorscale = NULL, manual_colors = NULL, nbreaks = 3, width = NULL, height = NULL) {
+
+  if (is.null(scale) & is.null(manual_colors)) {
+    stop("You must provide either a color scale or manual colors")
+  }
+
+  # check if variables is continuous or discrete
+  if (is.numeric(variables)) {
+    scale <- "continuous"
+    breaks <- pretty(variables, n = nbreaks)
+  } else {
+    scale <- "discrete"
+    variables <- as.factor(variables)
+    breaks <- levels(variables)
+  }
+
+  if (!is.null(manual_colors)) {
+    if (scale == "discrete") {
+      if (!all(names(manual_colors) %in% breaks)) {
+        stop("All levels of the variable must be present in the manual colors")
+      }
+      colors <- unname(manual_colors[breaks])
+    }
+  }
+
+  htmlwidgets::createWidget(
+    name = "ResponsiveGuides",
+    x = list(breaks = breaks, scale = scale, colors = colors),
+    width = width,
+    height = height,
+    package = "ResponsiveGuides"
+  )
+}
+
+
+#' @export
+ResponsiveGuidesOutput <- function(outputId, width = "100%", height = "400px") {
+  shinyWidgetOutput(outputId, "ResponsiveGuides", width, height, package = "ResponsiveGuides")
+}
+#' @export
+renderResponsiveGuides <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, ResponsiveGuidesOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/ResponsiveGuides.js
@@ -0,0 +1,48 @@
+HTMLWidgets.widget({
+
+  name: 'ResponsiveGuides',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        let id = el.id;
+        let guideDiv = document.createElement('div');
+        guideDiv.id = 'guideDiv-' + id;
+        guideDiv.className = 'ResponsiveGuides___guideDiv';
+        if (x.scale == 'discrete') {
+          breaks = x.breaks;
+          colors = x.colors;
+          for (let i = 0; i < breaks.length; i++) {
+            let guide = document.createElement('div');
+            guide.className = 'ResponsiveGuides___guide';
+            let colorBox = document.createElement('div');
+            colorBox.className = 'ResponsiveGuides___colorBox';
+            colorBox.style.backgroundColor = colors[i];
+            guide.appendChild(colorBox);
+            let label = document.createElement('div');
+            label.className = 'ResponsiveGuides___label';
+            label.innerHTML = breaks[i];
+            guide.appendChild(label);
+            guideDiv.appendChild(guide);
+          }
+          el.appendChild(guideDiv);
+        }
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
